dd160e89b01c6fd118246edc9e653660bd3aa8d9
Seba-SL <slazo@fi.uba.ar>
2024-08-14 15:56:15 -0300

    PrimerasPruebas

---
+++ PruebaUno/scripts/Araña.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("¡Hola, Godot!")
+
+func _physics_process(delta):
+	var direccion = Input.get_vector("alt_izq","alt_derecha","alt_arriba","alt_abajo" )
+
+  # Ajusta la velocidad del movimiento
+	var velocidad = 100  # Puedes ajustar esta velocidad según lo necesites
+
+	# Mueve el nodo basado en la dirección
+	position += direccion * velocidad * delta
