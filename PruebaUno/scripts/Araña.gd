20886111dd8287954615eb1d70f2ff27aae0c389
Seba-SL <slazo@fi.uba.ar>
2024-08-14 17:16:17 -0300

    limitesenTerreno

--- PruebaUno/scripts/Araña.gd
+++ PruebaUno/scripts/Araña.gd
@@ -8,7 +8,7 @@ func _physics_process(delta):
 	var direccion = Input.get_vector("alt_izq","alt_derecha","alt_arriba","alt_abajo" )
 
   # Ajusta la velocidad del movimiento
-	var velocidad = 100  # Puedes ajustar esta velocidad según lo necesites
+	var velocidad = 150  # Puedes ajustar esta velocidad según lo necesites
 
 	# Mueve el nodo basado en la dirección
 	position += direccion * velocidad * delta
