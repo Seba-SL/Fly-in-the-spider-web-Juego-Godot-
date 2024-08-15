9a5f7a4032e31c62dbd7df77ae8cbe2a3d6d7bc2
Seba-SL <slazo@fi.uba.ar>
2024-08-14 22:03:08 -0300

    Menu

--- PruebaUno/scripts/Araña.gd
+++ PruebaUno/scripts/Araña.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@export var bounce_factor: float = 1.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("¡Hola, Godot!")
@@ -8,7 +9,10 @@ func _physics_process(delta):
 	var direccion = Input.get_vector("alt_izq","alt_derecha","alt_arriba","alt_abajo" )
 
   # Ajusta la velocidad del movimiento
-	var velocidad = 150  # Puedes ajustar esta velocidad según lo necesites
+	var velocidad = 250  # Puedes ajustar esta velocidad según lo necesites
 
 	# Mueve el nodo basado en la dirección
 	position += direccion * velocidad * delta
+	
+	
+
--- PruebaUno/scripts/mosca.gd
+++ PruebaUno/scripts/mosca.gd
@@ -8,7 +8,7 @@ func _physics_process(delta):
 	var direccion = Input.get_vector("ui_left","ui_right","ui_up","ui_down" )
 
   # Ajusta la velocidad del movimiento
-	var velocidad = 500  # Puedes ajustar esta velocidad según lo necesites
+	var velocidad = 750  # Puedes ajustar esta velocidad según lo necesites
 
 	# Mueve el nodo basado en la dirección
 	position += direccion * velocidad * delta
