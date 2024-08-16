c4001823f0d3a1d21a200549948c115ddee66e05
Seba-SL <slazo@fi.uba.ar>
2024-08-16 11:39:40 -0300

    barraMejorOriginal2

--- PruebaUno/scripts/Araña.gd
+++ PruebaUno/scripts/Araña.gd
@@ -4,10 +4,8 @@ extends CharacterBody2D
 var max_health: int = 100
 var current_health: int = max_health
 
-# Referencia a la barra de vida
-@onready var health_bar: ProgressBar = $HealthBar
 func _ready():
-	update_health_bar()
+	$"../GUI/ProgressBar".value = max_health
 	print("¡Hola, Godot!")
 	pass
 	
@@ -29,19 +27,3 @@ func _on_volver_al_menu_pressed():
 	pass # Replace with function body.
 
 
-func take_damage(amount: int):
-	current_health -= amount
-	current_health = clamp(current_health, 0, max_health)
-	update_health_bar()
-	
-	if current_health <= 0:
-		die()
-
-func update_health_bar():
-	if health_bar:
-		health_bar.value = current_health
-		health_bar.max_value = max_health
-
-func die():
-	queue_free() # O cualquier otra lógica para manejar la muerte del personaje
-	
--- PruebaUno/scripts/mosca.gd
+++ PruebaUno/scripts/mosca.gd
@@ -7,7 +7,7 @@ var current_health: int = max_health
 @onready var health_bar: ProgressBar = $GUI/ProgressBar
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	update_health_bar()
+	$GUI/ProgressBar.value = max_health
 	print("¡Hola, Godot!")
 	pass
 	
@@ -25,20 +25,5 @@ func _physics_process(delta):
 	position += direccion * velocidad * delta
 	
 
-func take_damage(amount: int):
-	current_health -= amount
-	current_health = clamp(current_health, 0, max_health)
-	update_health_bar()
-	
-	if current_health <= 0:
-		die()
 
-func update_health_bar():
-	if health_bar:
-		health_bar.value = current_health
-		health_bar.max_value = max_health
-
-func die():
-	queue_free() # O cualquier otra lógica para manejar la muerte del personaj
-	
 	
