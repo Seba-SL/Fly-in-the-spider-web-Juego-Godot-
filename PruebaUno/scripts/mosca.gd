a1cce3ee6c92f56ed77c2c85063a6ba995d5e0fa
Seba-SL <slazo@fi.uba.ar>
2024-08-16 09:35:36 -0300

    barrasvida

--- PruebaUno/scripts/mosca.gd
+++ PruebaUno/scripts/mosca.gd
@@ -1,8 +1,17 @@
 extends CharacterBody2D
 
+@export var salud = 5 
+@onready var barra = $GUI/ProgressBar
+var inmune : bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	barra.max_value = salud
 	print("¡Hola, Godot!")
+	
+func _process(delta):
+	daño_control()
+	pass
 
 func _physics_process(delta):
 	var direccion = Input.get_vector("ui_left","ui_right","ui_up","ui_down" )
@@ -12,3 +21,16 @@ func _physics_process(delta):
 
 	# Mueve el nodo basado en la dirección
 	position += direccion * velocidad * delta
+	
+	
+func daño_control():
+	if inmune == true:
+		if salud > 0:
+			salud -= 1
+			barra.value = salud 
+			inmune = false
+		if salud <= 0:
+			get_tree().reload_current_scene()
+	pass
+	
+	
